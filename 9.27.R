80f89f8cc7d34a98bdbe20266bc2e38543584eb5
Jing Xu <jingxu21@bu.edu>
2022-09-27 19:18:39 -0400

    ic

--- 9.27.R
+++ 9.27.R
@@ -2,8 +2,9 @@ library(tidyverse)
 library()
 
 
-npy <- function({})
-  
+npy <- function(cfs,r){
+  sum(cfs/(1+r)^(1:ny))
+}
 ny <- 5
 in_m_size <- 2
 on_m_share <- 0.08
@@ -11,11 +12,18 @@ rd <- 700
 ct <- 500
 gm_size <- 1.03
 gm_share <- 1.2
-u_rev <- (140-30)*12
+u_rev <- (130-40)*12
 dscr <- 1.09
 df <- data.frame(
   year = 1:ny) %>%
   mutate(
-    m_size = in_m_size*gm_size^(year-1),
-    m_share = in_m_share*gm_share^(year-1)
+    sales = in_m_size*gm_size^(year-1)*in_m_share*gm_share^(year-1),
+    rev = u_rev*sales,
+    cum_pft = cumsum(rev)-rd-ct
     )
+
+cfs <- df$rev
+cfs
+
+
+net_present_value <- npy(df$rev, r)-cd-ct
